f6250f43e8cf9fa668d5af5fe8d71c03e0709fd9
Trung Hiếu Lê <hieult1409@gmail.com>
2018-11-05 12:22:54 +0200

    Update Heroku deploy

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,11 +11,19 @@ public class App {
     public static void main( String[] args ) {
 
         // Configure Spark
-        port(4567);
+        port(getHerokuAssignedPort());
         staticFiles.location("/public");
 
         // Routes
         post("/hash", handleHashPost);
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
     // The class representing the request body
